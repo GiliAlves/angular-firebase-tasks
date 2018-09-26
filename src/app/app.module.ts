abf5b40ad98ac85cc4b2df973ff1bead2321aa83
GiliAlves <gld.7@hotmail.com>
2018-09-25 22:41:16 -0300

    continuacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,58 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { MatToolbarModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatListModule,
+  MatLineModule,
+  MatSlideToggleModule,
+  MatButtonModule,
+  MatIconModule,
+  MatDialogModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatMenuModule
+} from '@angular/material';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment.prod';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskService } from './task.service';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent,
+    TaskListComponent,
+    TaskDialogComponent
+  ],
+  entryComponents: [
+    TaskDialogComponent
   ],
   imports: [
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    FormsModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatLineModule,
+    MatListModule,
+    MatMenuModule,
     MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    TaskService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task.model';
+import { Observable } from 'rxjs/Observable';
+import { TaskService } from '../task.service';
+import { MatDialog, MatDialogConfig } from '@angular/material';
+import { TaskDialogComponent } from '../task-dialog/task-dialog.component';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  tasks$: Observable<Task[]>;
+  selectedTask: Task;
+
+  constructor(
+    private dialog: MatDialog,
+    private taskService: TaskService
+  ) { }
+
+  ngOnInit(): void {
+    this.tasks$ = this.taskService.tasks.valueChanges();
+  }
+
+  onPerfomTask(task: Task): void {
+    task.done = !task.done;
+    this.taskService.update(task);
+  }
+
+  showDialog(task?: Task): void {
+    const config: MatDialogConfig<any> = (task) ? {data: { task }} : null;
+    this.dialog.open(TaskDialogComponent, config);
+  }
+}
